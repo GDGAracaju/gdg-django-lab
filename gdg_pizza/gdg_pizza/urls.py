41d89b99d8bb66b34c3c7ab3c0eca878f8b1182a
Erick Mendonça <erickmendonca@yahoo.com.br>
2014-05-21 03:47:41 +0000

    Adiciona uma página index

--- gdg_pizza/gdg_pizza/urls.py
+++ gdg_pizza/gdg_pizza/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
+import views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', views.index, name='index')
     # Examples:
     # url(r'^$', 'gdg_pizza.views.home', name='home'),
     # url(r'^gdg_pizza/', include('gdg_pizza.foo.urls')),
