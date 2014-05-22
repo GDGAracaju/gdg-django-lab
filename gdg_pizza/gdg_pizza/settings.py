085c19a76b940c03c1adfb9b0999540384840b7c
Erick Mendonça <erickmendonca@yahoo.com.br>
2014-05-22 20:04:31 +0000

    Adiciona modelo Product

--- gdg_pizza/gdg_pizza/settings.py
+++ gdg_pizza/gdg_pizza/settings.py
@@ -11,13 +11,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gdg_pizza',
+        'HOST': '0.0.0.0',
+        'PORT': '',
     }
 }
 
