7be56405aec4f70d0cb3f14e3ccc6d9cb0e173df
Erick Mendonça <erickmendonca@yahoo.com.br>
2014-05-23 19:52:29 +0000

    Adiciona modelo e config de banco de dados

--- gdg_pizza/cardapio/models.py
+++ gdg_pizza/cardapio/models.py
@@ -1,7 +1,16 @@
+# coding=UTF-8
+import datetime
 from django.db import models
 
-class Product (models.Model)
+class Product (models.Model):
 	name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    description = models.TextField(blank=True)
-    featured = models.BooleanField(default=False, blank=True)
+	price = models.DecimalField(max_digits=6, decimal_places=2)
+	description = models.TextField(blank=True)
+	featured = models.BooleanField(default=False, blank=True)
+	pub_date = models.DateTimeField('data de publicação', auto_now_add=True)
+
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+	def __unicode__(self):
+		return self.name
