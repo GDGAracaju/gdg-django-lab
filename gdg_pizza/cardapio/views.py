907e0ef1d9ab4a2f68d54a66dea429f46934946e
Erick Mendonça <erickmendonca@yahoo.com.br>
2014-06-04 18:13:36 +0000

    Adiciona templates index e detail

--- gdg_pizza/cardapio/views.py
+++ gdg_pizza/cardapio/views.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 # coding: UTF-8
 # Create your views here.
+from cardapio.models import Category
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 def cardapio_index(request):
-    return HttpResponse("Olá! Bem vindo ao cardápio.")
+    latest_category_list = Category.objects.order_by('-created_on')[:5]
+    context = {'latest_category_list': latest_category_list}
+    return render(request, 'cardapio/index.html', context)
 
 def detail(request, category_id):
-    return HttpResponse(u"Você está vendo detalhes da categoria %s." % category_id)
+    category = get_object_or_404(Category, pk=category_id)
+    return render(request, 'cardapio/detail.html', {'category': category})
 
 def products(request, category_id):
     return HttpResponse(u"Você está vendo a lista de produtos da categoria %s." % category_id)
