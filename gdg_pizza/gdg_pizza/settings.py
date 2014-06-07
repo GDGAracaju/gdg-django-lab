12ad278944a714fcdb36a143d07e17b0704edf9b
Erick Mendonça <erickmendonca@yahoo.com.br>
2014-06-07 02:38:23 +0000

    Adiciona template com Bootstrap

--- gdg_pizza/gdg_pizza/settings.py
+++ gdg_pizza/gdg_pizza/settings.py
@@ -55,11 +55,14 @@ MEDIA_ROOT = ''
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'
+
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -67,6 +70,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -104,8 +108,6 @@ ROOT_URLCONF = 'gdg_pizza.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'gdg_pizza.wsgi.application'
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 TEMPLATE_DIRS = (
 	os.path.join(BASE_DIR, 'templates')
