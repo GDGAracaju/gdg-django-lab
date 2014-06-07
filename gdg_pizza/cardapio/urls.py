137715e55337084bb6ef2dcac79906a17d7fe054
Erick Mendonça <erickmendonca@yahoo.com.br>
2014-06-07 03:32:02 +0000

    Bootstrap e forms

--- gdg_pizza/cardapio/urls.py
+++ gdg_pizza/cardapio/urls.py
@@ -5,4 +5,6 @@ from cardapio import views
 urlpatterns = patterns('',
     url(r'^$', views.cardapio_index, name='cardapio_index'),
 	url(r'^(?P<category_id>\d+)/$', views.detail, name='detail'),
+	url(r'^add/$', views.add, name='add'),
+	url(r'^add_category/$', views.add_category, name='add_category')
 )
--- gdg_pizza/cardapio/views.py
+++ gdg_pizza/cardapio/views.py
@@ -2,14 +2,27 @@
 # coding: UTF-8
 # Create your views here.
 from cardapio.models import Category
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404
 
 def cardapio_index(request):
-    latest_category_list = Category.objects.order_by('-created_on')[:5]
+    latest_category_list = Category.objects.order_by('name')
     context = {'latest_category_list': latest_category_list}
     return render(request, 'cardapio/index.html', context)
 
 def detail(request, category_id):
     category = get_object_or_404(Category, pk=category_id)
     return render(request, 'cardapio/detail.html', {'category': category})
+
+def add(request):
+    return render(request, 'cardapio/add.html')
+
+def add_category(request):
+	c = Category()
+	try:
+		c.name = request.POST['category']
+		c.save()
+	except:
+		return render(request, 'cardapio/add.html', { 'error_message': 'Algo errado :-(' })
+	return HttpResponseRedirect(reverse('cardapio:cardapio_index'))
